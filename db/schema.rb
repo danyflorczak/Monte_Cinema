44b6e5873d9b1900854ec1a0b1f9efca435d126a
daniel florczak <danyflorczak@gmail.com>
2022-09-13 13:30:12 +0200

    Creating movie table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_112102) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_112745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.float "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "screenings", force: :cascade do |t|
     t.datetime "start_time", null: false
