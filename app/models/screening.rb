8c17b21da7c324872be48ebb6ecc27ab16bad9ea
daniel florczak <danyflorczak@gmail.com>
2022-09-19 19:52:57 +0200

    Adding available seats method

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -3,7 +3,17 @@
 class Screening < ApplicationRecord
   belongs_to :movie
   belongs_to :hall
+  has_many :reservations
   validates_associated :movie, :hall
   validates :start_time, :end_time, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+  def available_seats
+    taken_seats = []
+    reservations.each do |r|
+      r.tickets.each do |t|
+        taken_seats.push(t.seat)
+      end
+    end
+    hall.seats_array - taken_seats
+  end
 end
