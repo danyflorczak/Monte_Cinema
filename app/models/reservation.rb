0efa50f52aa984ab67ec16395f5596e002beb5da
daniel florczak <danyflorczak@gmail.com>
2022-09-21 10:06:11 +0200

    fix: small fix in reservation statuses

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Reservation < ApplicationRecord
-  enum status: { created: 0, paid: 1, canceled: 3 }
+  enum status: { created: 0, paid: 1, canceled: 2 }
   belongs_to :screening
   has_many :tickets, dependent: :destroy
 end
