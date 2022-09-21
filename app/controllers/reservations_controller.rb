1e5ce1994f42dd6a2f7b8132102539ef0c8cee69
daniel florczak <danyflorczak@gmail.com>
2022-09-21 10:36:17 +0200

    add: Creating index, edit, update and destroy actions in reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,9 +2,16 @@
 
 class ReservationsController < ApplicationController
   before_action :set_screening, only: %i[new create]
+  before_action :set_reservation, only: %i[edit update destroy]
+  def index
+    @reservations = Reservation.all
+  end
+
   def new
     @reservation = Reservation.new
   end
+
+  def edit; end
 
   def create
     @reservation = Reservation.new(screening_id: params[:screening_id], status: :created)
@@ -19,15 +26,36 @@ class ReservationsController < ApplicationController
     end
   end
 
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path, notice: 'Reservation status was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    redirect_to reservations_url, notice: 'Reservation was successfully deleted.'
+  end
+
   private
 
   def set_screening
     @screening = Screening.find(params[:screening_id])
   end
 
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
   def create_tickets
     params[:seats].each do |seat|
       Ticket.create(reservation_id: @reservation.id, seat:)
     end
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:screening_id, :ticket_id, :status)
   end
 end
