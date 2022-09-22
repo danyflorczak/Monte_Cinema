c3d685736acd52e0b9ccbf0bd01e338b772d8ea1
daniel florczak <danyflorczak@gmail.com>
2022-09-22 13:58:26 +0200

    add: Creating policy for updating hall

---
+++ app/policies/hall_policy.rb
@@ -0,0 +1,16 @@
+class HallPolicy < ApplicationPolicy
+
+  def create?
+    manager?
+  end
+
+  def update?
+    manager?
+  end
+
+  private
+  
+  def manager?
+    user.role == "manager"
+  end
+end
