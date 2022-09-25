8b2eb84f32ef13ccfb40733ba609141ce6a1ed27
daniel florczak <danyflorczak@gmail.com>
2022-09-25 18:41:24 +0200

    add: adding dependent destroy and description presence true

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
-  attribute :duration, :string
-  has_many :screenings
+  has_many :screenings, dependent: :destroy
   has_many :halls, through: :screenings
-  validates :title, :duration, presence: true
+  validates_associated :screenings, :halls
+  validates :title, :description, :duration, presence: true
+  validates :title, length: { maximum: 255, too_long: '%<count>s characters is the maximum allowed' }
+  validates :description, length: { maximum: 30_000, too_long: '%<count>s characters is the maximum allowed' }
+  validates :duration, numericality: { only_integer: true, greater_than: 0 }
 end
