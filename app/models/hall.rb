09cec724baa5647628ed77e7e90ce4803fd80dc8
daniel florczak <danyflorczak@gmail.com>
2022-09-25 18:39:18 +0200

    add: adding validation of associated models

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -3,6 +3,7 @@
 class Hall < ApplicationRecord
   has_many :screenings
   has_many :movies, through: :screenings
+  validates_associated :screenings, :movies
   validates :name, :capacity, presence: true
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
   validates :name, uniqueness: true
