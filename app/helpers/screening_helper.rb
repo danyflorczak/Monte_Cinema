5c70a7780e553ccd32ee27f273779a5bcce7be2e
daniel florczak <danyflorczak@gmail.com>
2022-09-27 14:11:54 +0200

    fix: deleting comment from screening helper

--- app/helpers/screening_helper.rb
+++ app/helpers/screening_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module ScreeningHelper
+  DAYS_NUMBER = 7
   def create_date_links
     content_tag(:ul, class: 'd-flex mb-3') do
-      # creating links for 7 days of a week
-      7.times do |num|
+      DAYS_NUMBER.times do |num|
         concat(link_to((Time.zone.today + num.days).strftime('%a %d/%m'),
                        movies_path(start_time: Time.zone.today + num.days), class: 'mx-2'))
       end
