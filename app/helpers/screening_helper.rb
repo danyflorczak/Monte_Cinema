46a54b788ce77c5fca3d90ff8821162575955812
daniel florczak <danyflorczak@gmail.com>
2022-09-27 09:59:08 +0200

    fix: screening helper adding comment and name changes

--- app/helpers/screening_helper.rb
+++ app/helpers/screening_helper.rb
@@ -3,6 +3,7 @@
 module ScreeningHelper
   def create_date_links
     content_tag(:ul, class: 'd-flex mb-3') do
+      # creating links for 7 days of a week
       7.times do |num|
         concat(link_to((Time.zone.today + num.days).strftime('%a %d/%m'),
                        movies_path(start_time: Time.zone.today + num.days), class: 'mx-2'))
@@ -10,7 +11,7 @@ module ScreeningHelper
     end
   end
 
-  def which_day?(obj)
+  def which_day?(screening)
     link_date = params[:start_time] || DateTime.now.strftime('%Y-%m-%d')
     obj.start_time.strftime('%Y-%m-%d') == link_date
   end
