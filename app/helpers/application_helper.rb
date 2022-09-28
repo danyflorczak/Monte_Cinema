a3b198469a47cf9d388d3cb63e07920412c05e13
daniel florczak <danyflorczak@gmail.com>
2022-09-29 00:12:13 +0200

    add: adding validation of user role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
   def format_date(date)
     date.strftime('%d/%m/%Y %H:%M')
   end
+
+  def manager?
+    current_user.role == 'manager'
+  end
+
+  def client?
+    current_user.role == 'client'
+  end
 end
