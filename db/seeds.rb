b3ae21bf0cc2166f48ccf6874b759cb4ad9b4d7e
daniel florczak <danyflorczak@gmail.com>
2022-09-28 12:09:50 +0200

    fix: making seeds more readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ hall_capacities = [20, 50, 100, 200]
 10.times do
   Hall.create(
     name: Faker::FunnyName.name,
-    capacity: hall_capacities[rand(4)]
+    capacity: hall_capacities.sample
   )
 end
 10.times do
@@ -22,7 +22,7 @@ end
   Screening.create(
     movie_id: Movie.all.sample.id,
     hall_id: Hall.all.sample.id,
-    start_time: DateTime.current.beginning_of_minute + rand(7).days + rand(24).hours + rand(60).minutes,
+    start_time: rand(DateTime.current .. DateTime.current + 7.days),
     price: rand(15..24)
   )
 end
