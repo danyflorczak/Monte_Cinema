850f37843facc18d403f0c6aebe4ac261be9655a
daniel florczak <danyflorczak@gmail.com>
2022-10-01 16:56:45 +0200

    add: validating uniquness of movie title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@ class Movie < ApplicationRecord
   has_many :halls, through: :screenings
   validates_associated :screenings, :halls
   validates :title, :description, :duration, presence: true
+  validates :title, uniqueness: true
   validates :description, length: { maximum: 1000, too_long: '%<count>s characters is the maximum allowed' }
   validates :duration, numericality: { greater_than: 0 }
 end
