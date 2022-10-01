99bf1eb003a0fe6bcb198a0311645a87bafc205f
daniel florczak <danyflorczak@gmail.com>
2022-10-01 16:57:17 +0200

    fix: fixing with rubocop

--- spec/factories/hall.rb
+++ spec/factories/hall.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :hall do
     name { 'My name' }
-    capacity { Faker::Number.number(digits: 2).to_i } 
+    capacity { Faker::Number.number(digits: 2).to_i }
   end
 end
