ff40a55590ba9ef18f7d2e8d78bf3dde99baaa05
daniel florczak <danyflorczak@gmail.com>
2022-10-04 16:25:46 +0200

    add: email to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_928_220_606) do
+ActiveRecord::Schema[7.0].define(version: 20_221_004_142_412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_928_220_606) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.bigint 'user_id'
+    t.string 'email', null: false
     t.index ['screening_id'], name: 'index_reservations_on_screening_id'
     t.index ['user_id'], name: 'index_reservations_on_user_id'
   end
@@ -68,6 +69,10 @@ ActiveRecord::Schema[7.0].define(version: 20_220_928_220_606) do
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
     t.integer 'role', default: 0, null: false
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
