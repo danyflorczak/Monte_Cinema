12a3a12b172a886deb025ae61e3608fc7f445fa4
daniel florczak <danyflorczak@gmail.com>
2022-10-06 16:13:15 +0200

    add: creating basic views for sent emails

--- spec/mailers/previews/reservation_mailer_preview.rb
+++ spec/mailers/previews/reservation_mailer_preview.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/reservation_mailer
 class ReservationMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/reservation_mailer/reservation_created
   def reservation_created
     ReservationMailer.reservation_created
   end
-
 end
