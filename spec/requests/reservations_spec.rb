ce9e442e59a72b362871c723938e4e8843cedbe3
daniel florczak <danyflorczak@gmail.com>
2022-10-09 17:41:52 +0200

    tests: index and new reservations requests

---
+++ spec/requests/reservations_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe '/halls', type: :request do
+  let(:user) { create :user }
+  let(:manager) { create :user, email: 'admin@gmail.com', role: :manager }
+  describe 'GET /reservations' do
+    context 'when user not signed in' do
+      it 'redirects to sign_in' do
+        get('/reservations')
+        expect(response).to redirect_to('/users/sign_in')
+      end
+
+      it 'returns redirect status' do
+        get('/reservations')
+        expect(response.status).to eq(302)
+      end
+    end
+
+  end
+
+  context 'when user with permission' do
+    before { sign_in manager }
+    it 'returns successful response' do
+      get('/reservations')
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders proper template' do
+      get('/reservations')
+      expect(response).to render_template('reservations/index')
+    end
+  end
+
+  describe 'GET screenings/screening_id/reservations/new' do
+    let(:screening) {create :screening}
+    it 'returns successful response' do
+      get("/screenings/#{screening.id}/reservations/new")
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders proper template' do
+      get("/screenings/#{screening.id}/reservations/new")
+      expect(response).to render_template('reservations/new')
+    end
+  end
+
+end
