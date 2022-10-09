d8fb6ee7ff80e95850d27b92ee19266fd9793b55
daniel florczak <danyflorczak@gmail.com>
2022-10-09 14:47:20 +0200

    tests: creating cancel reservation spec

---
+++ spec/services/reservations/cancel_reservation_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Reservations::CancelReservation do
+
+  describe '#call' do
+    context 'when reservation is booked' do
+      let!(:reservation) { create :reservation}
+      subject { described_class.new(reservation).call }
+
+      it 'updates status to canceled' do
+        expect { subject }.to(change { reservation.status }.from('booked').to('canceled'))
+      end
+
+    end
+
+     context 'when reservation status is confirmed' do
+      let!(:reservation) { create :reservation, status: :confirmed}
+      subject { described_class.new(reservation).call }
+
+      it 'does not update status' do
+        expect { subject }.to_not(change { reservation.status })
+      end
+
+    end
+
+    context 'when reservation status is canceled' do
+      let!(:reservation) { create :reservation, status: :canceled}
+      subject { described_class.new(reservation).call }
+
+      it 'does not update status' do
+        expect { subject }.to_not(change { reservation.status })
+      end
+
+    end
+
+  end
+end
