3b538f21f09373d97f933045492e19fcdf41bc44
daniel florczak <danyflorczak@gmail.com>
2022-10-12 12:25:17 +0200

    add: sengrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'monte@cinema.com'
+  default from: 'https://monte-cinema-v1.herokuapp.com/'
   layout 'mailer'
 end
