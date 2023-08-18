c059e01a748ddfb499955ff7cc0f60cf25298720
Daniel Florczak <danyflorczak@gmail.com>
2023-08-18 19:25:24 +0200

    validate capacity presence in hall spec

--- spec/models/hall_spec.rb
+++ spec/models/hall_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Hall, type: :model do
 
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :capacity }
     it { should validate_uniqueness_of :name }
     it { should validate_numericality_of(:capacity).is_greater_than(0) }
   end
