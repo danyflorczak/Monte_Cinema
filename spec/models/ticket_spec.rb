a4676dac2fbb2547132338aca107ad7fd5e79aca
Daniel Florczak <danyflorczak@gmail.com>
2023-08-18 20:11:18 +0200

    ticket model spec

---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Ticket, type: :model do
+  before do
+    create(:ticket)
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:reservation) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :seat }
+  end
+end
