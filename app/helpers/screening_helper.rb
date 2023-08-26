aa9ec2e720f740d30c4f8f0715f2ddd15293d37b
Daniel Florczak <danyflorczak@gmail.com>
2023-08-26 21:25:46 +0200

    new style for date links

--- app/helpers/screening_helper.rb
+++ app/helpers/screening_helper.rb
@@ -3,10 +3,10 @@
 module ScreeningHelper
   DAYS_NUMBER = 7
   def create_date_links
-    content_tag(:ul, class: "d-flex mb-3") do
+    content_tag(:ul, class: "d-flex mb-3 date-links") do
       DAYS_NUMBER.times do |num|
         concat(link_to((Time.zone.today + num.days).strftime("%a %d/%m"),
-          request.params.merge(start_time: Time.zone.today + num.days), class: "mx-2",))
+          request.params.merge(start_time: Time.zone.today + num.days), class: "mx-2 p-4 fs-5 fw-normal",))
       end
     end
   end
