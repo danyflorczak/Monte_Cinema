00212b532978e19a2f91c527a569e2ba676ac9cf
Daniel Florczak <danyflorczak@gmail.com>
2023-08-27 21:51:47 +0200

    add director, genre and release date to movies model

---
+++ db/migrate/20230827112853_add_properties_to_movies.rb
@@ -0,0 +1,9 @@
+class AddPropertiesToMovies < ActiveRecord::Migration[7.0]
+  def change
+    change_table :movies do |t|
+      t.string :genre
+      t.string :director
+      t.datetime :release_date
+    end
+  end
+end
