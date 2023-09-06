dc7f6d77fbc9c3ffb86ddfe865f214feb32360ed
Daniel Florczak <danyflorczak@gmail.com>
2023-09-06 14:33:53 +0200

    changing line item properties

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CheckoutsController < ApplicationController
   before_action :authenticate_user!
 
@@ -6,15 +8,44 @@ class CheckoutsController < ApplicationController
     current_user.payment_processor.customer
 
     @checkout_session = current_user
-          .payment_processor
-          .checkout(
-            mode: "payment",
-            line_items: "price_1NmXHaLHG4welAl56nzmBuV0",
-            success_url: checkout_success_url,
-          )
+      .payment_processor
+      .checkout(
+        mode: "payment",
+        line_items: [{
+          price_data: {
+            currency: "pln",
+            product_data: {
+              name: "Film",
+            },
+            unit_amount: 300,
+          },
+          quantity: 1,
+        }],
+        success_url: checkout_success_url,
+      )
   end
 
-  def success
-
+  def create
+    session = Stripe::Checkout::Session.create({
+      payment_method_types: ["card"],
+      line_items: [{
+        price_data: {
+          currency: "pln",
+          product_data: {
+            name: "Film",
+          },
+          unit_amount: 300,
+        },
+        quantity: 1,
+      }],
+      mode: "payment",
+      success_url: "https://example.com/success",
+      cancel_url: "https://example.com/cancel",
+    })
+    respond_to do |format|
+      format.html
+    end
   end
+
+  def success; end
 end
