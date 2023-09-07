15d0c08f2cacee013e2fe8637911d507b3268ac7
Daniel Florczak <danyflorczak@gmail.com>
2023-09-07 22:52:35 +0200

    fix tickets spec and delete checkout spec

--- spec/requests/tickets_spec.rb
+++ spec/requests/tickets_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Tickets", type: :request do
+  let(:reservation) { create :reservation }
   describe "GET /index" do
     it "returns http success" do
-      get "/tickets/index"
+      get reservation_tickets_url(reservation)
       expect(response).to have_http_status(:success)
     end
   end
