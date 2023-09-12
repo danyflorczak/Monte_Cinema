ba0449b08f1ab067f4ac76125bccb6b87ba9b97a
Daniel Florczak <danyflorczak@gmail.com>
2023-09-12 13:26:42 +0200

    add new locales messages for movies halls and not authorized user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i(show edit update destroy)
-  before_action :authenticate_user!, except: %i(index show)
+  before_action :set_movie, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
     authorize Movie
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
     authorize Movie
     @movie = ::Movies::Create.new(movie_params).call
     if @movie.valid?
-      redirect_to movie_url(@movie), notice: "Movie was successfully created."
+      redirect_to movie_url(@movie), notice: I18.t("movie.create")
     else
       render :new, status: :unprocessable_entity
     end
@@ -40,14 +40,14 @@ class MoviesController < ApplicationController
     if @movie.errors.any?
       render :edit, status: :unprocessable_entity
     else
-      redirect_to movie_url(@movie), notice: "Movie was successfully updated."
+      redirect_to movie_url(@movie), notice: I18.t("movie.update")
     end
   end
 
   def destroy
     authorize Movie
     ::Movies::Delete.new(params[:id]).call
-    redirect_to movies_url, notice: "Movie was successfully destroyed."
+    redirect_to movies_url, notice: I18.t("movie.destroy")
   end
 
   private
@@ -58,6 +58,6 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :description, :duration, :poster_image, :director, :release_date, :cast,
-      :genre,)
+                                  :genre)
   end
 end
